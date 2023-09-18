005d0215df9e24ca65ad4dcb753fa70031a84e77
pentagramsandsunshine <82670340+pentagramsandsunshine@users.noreply.github.com>
2023-09-18 17:32:32 -0400

    Update forTesting.py

--- forTesting.py
+++ forTesting.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.by import By
+from selenium.webdriver.common.by import Keys
 import time
 
 options = webdriver.ChromeOptions()
